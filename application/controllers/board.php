0ec337d05c17f7d9ea156ea9033374b0297cacb9
machoman1337 <comicnabster@gmail.com>
2014-03-19 20:23:43 -0400

    starter code

---
+++ application/controllers/board.php
@@ -0,0 +1,143 @@
+<?php
+
+class Board extends CI_Controller {
+     
+    function __construct() {
+    		// Call the Controller constructor
+	    	parent::__construct();
+	    	session_start();
+    } 
+          
+    public function _remap($method, $params = array()) {
+	    	// enforce access control to protected functions	
+    		
+    		if (!isset($_SESSION['user']))
+   			redirect('account/loginForm', 'refresh'); //Then we redirect to the index page again
+ 	    	
+	    	return call_user_func_array(array($this, $method), $params);
+    }
+    
+    
+    function index() {
+		$user = $_SESSION['user'];
+    		    	
+	    	$this->load->model('user_model');
+	    	$this->load->model('invite_model');
+	    	$this->load->model('match_model');
+	    	
+	    	$user = $this->user_model->get($user->login);
+
+	    	$invite = $this->invite_model->get($user->invite_id);
+	    	
+	    	if ($user->user_status_id == User::WAITING) {
+	    		$invite = $this->invite_model->get($user->invite_id);
+	    		$otherUser = $this->user_model->getFromId($invite->user2_id);
+	    	}
+	    	else if ($user->user_status_id == User::PLAYING) {
+	    		$match = $this->match_model->get($user->match_id);
+	    		if ($match->user1_id == $user->id)
+	    			$otherUser = $this->user_model->getFromId($match->user2_id);
+	    		else
+	    			$otherUser = $this->user_model->getFromId($match->user1_id);
+	    	}
+	    	
+	    	$data['user']=$user;
+	    	$data['otherUser']=$otherUser;
+	    	
+	    	switch($user->user_status_id) {
+	    		case User::PLAYING:	
+	    			$data['status'] = 'playing';
+	    			break;
+	    		case User::WAITING:
+	    			$data['status'] = 'waiting';
+	    			break;
+	    	}
+	    	
+		$this->load->view('match/board',$data);
+    }
+
+ 	function postMsg() {
+ 		$this->load->library('form_validation');
+ 		$this->form_validation->set_rules('msg', 'Message', 'required');
+ 		
+ 		if ($this->form_validation->run() == TRUE) {
+ 			$this->load->model('user_model');
+ 			$this->load->model('match_model');
+
+ 			$user = $_SESSION['user'];
+ 			 
+ 			$user = $this->user_model->getExclusive($user->login);
+ 			if ($user->user_status_id != User::PLAYING) {	
+				$errormsg="Not in PLAYING state";
+ 				goto error;
+ 			}
+ 			
+ 			$match = $this->match_model->get($user->match_id);			
+ 			
+ 			$msg = $this->input->post('msg');
+ 			
+ 			if ($match->user1_id == $user->id)  {
+ 				$msg = $match->u1_msg == ''? $msg :  $match->u1_msg . "\n" . $msg;
+ 				$this->match_model->updateMsgU1($match->id, $msg);
+ 			}
+ 			else {
+ 				$msg = $match->u2_msg == ''? $msg :  $match->u2_msg . "\n" . $msg;
+ 				$this->match_model->updateMsgU2($match->id, $msg);
+ 			}
+ 				
+ 			echo json_encode(array('status'=>'success'));
+ 			 
+ 			return;
+ 		}
+		
+ 		$errormsg="Missing argument";
+ 		
+		error:
+			echo json_encode(array('status'=>'failure','message'=>$errormsg));
+ 	}
+ 
+	function getMsg() {
+ 		$this->load->model('user_model');
+ 		$this->load->model('match_model');
+ 			
+ 		$user = $_SESSION['user'];
+ 		 
+ 		$user = $this->user_model->get($user->login);
+ 		if ($user->user_status_id != User::PLAYING) {	
+ 			$errormsg="Not in PLAYING state";
+ 			goto error;
+ 		}
+ 		// start transactional mode  
+ 		$this->db->trans_begin();
+ 			
+ 		$match = $this->match_model->getExclusive($user->match_id);			
+ 			
+ 		if ($match->user1_id == $user->id) {
+			$msg = $match->u2_msg;
+ 			$this->match_model->updateMsgU2($match->id,"");
+ 		}
+ 		else {
+ 			$msg = $match->u1_msg;
+ 			$this->match_model->updateMsgU1($match->id,"");
+ 		}
+
+ 		if ($this->db->trans_status() === FALSE) {
+ 			$errormsg = "Transaction error";
+ 			goto transactionerror;
+ 		}
+ 		
+ 		// if all went well commit changes
+ 		$this->db->trans_commit();
+ 		
+ 		echo json_encode(array('status'=>'success','message'=>$msg));
+		return;
+		
+		transactionerror:
+		$this->db->trans_rollback();
+		
+		error:
+		echo json_encode(array('status'=>'failure','message'=>$errormsg));
+ 	}
+ 	
+ }
+
---
+++ application/views/match/board.php
@@ -0,0 +1,87 @@
+
+<!DOCTYPE html>
+
+<html>
+	<head>
+	<script src="http://code.jquery.com/jquery-latest.js"></script>
+	<script src="<?= base_url() ?>/js/jquery.timers.js"></script>
+	<script>
+
+		var otherUser = "<?= $otherUser->login ?>";
+		var user = "<?= $user->login ?>";
+		var status = "<?= $status ?>";
+		
+		$(function(){
+			$('body').everyTime(2000,function(){
+					if (status == 'waiting') {
+						$.getJSON('<?= base_url() ?>arcade/checkInvitation',function(data, text, jqZHR){
+								if (data && data.status=='rejected') {
+									alert("Sorry, your invitation to play was declined!");
+									window.location.href = '<?= base_url() ?>arcade/index';
+								}
+								if (data && data.status=='accepted') {
+									status = 'playing';
+									$('#status').html('Playing ' + otherUser);
+								}
+								
+						});
+					}
+					var url = "<?= base_url() ?>board/getMsg";
+					$.getJSON(url, function (data,text,jqXHR){
+						if (data && data.status=='success') {
+							var conversation = $('[name=conversation]').val();
+							var msg = data.message;
+							if (msg.length > 0)
+								$('[name=conversation]').val(conversation + "\n" + otherUser + ": " + msg);
+						}
+					});
+			});
+
+			$('form').submit(function(){
+				var arguments = $(this).serialize();
+				var url = "<?= base_url() ?>board/postMsg";
+				$.post(url,arguments, function (data,textStatus,jqXHR){
+						var conversation = $('[name=conversation]').val();
+						var msg = $('[name=msg]').val();
+						$('[name=conversation]').val(conversation + "\n" + user + ": " + msg);
+						});
+				return false;
+				});	
+		});
+	
+	</script>
+	</head> 
+<body>  
+	<h1>Game Area</h1>
+
+	<div>
+	Hello <?= $user->fullName() ?>  <?= anchor('account/logout','(Logout)') ?>  
+	</div>
+	
+	<div id='status'> 
+	<?php 
+		if ($status == "playing")
+			echo "Playing " . $otherUser->login;
+		else
+			echo "Wating on " . $otherUser->login;
+	?>
+	</div>
+	
+<?php 
+	
+	echo form_textarea('conversation');
+	
+	echo form_open();
+	echo form_input('msg');
+	echo form_submit('Send','Send');
+	echo form_close();
+	
+?>
+	
+	
+	
+	
+</body>
+
+</html>
+
