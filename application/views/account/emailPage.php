ead03b6e0784dcbbf1370e13dc225e070624aeec
machoman1337 <comicnabster@gmail.com>
2014-03-29 20:27:46 -0400

    update README file

--- application/views/account/emailPage.php
+++ application/views/account/emailPage.php
@@ -1,24 +1,18 @@
-
 <!DOCTYPE html>
 <html>
 <head>
-	<style>
-		input {
-			display: block;
-		}
-	</style>
-
+<link href="<?echo base_url();?>css/template.css" rel="stylesheet" type="text/css"/>
 </head> 
 <body>  
-	<h1>Password Recovery</h1>
-	<p>Please check your email for your new password.</p>
-	
-	<?php 
-		if (isset($errorMsg)) {
-			echo "<p>" . $errorMsg . "</p>";
-		}
+<h1>Password Recovery</h1>
+<p>Please check your email for your new password.</p>
 
-		echo "<p>" . anchor('account/index','Login') . "</p>";
-	?>	
+<?php 
+	if (isset($errorMsg)) {
+		echo "<p>" . $errorMsg . "</p>";
+	}
+
+	echo "<p>" . anchor('account/index','Login') . "</p>";
+?>	
 </body>
 </html>
